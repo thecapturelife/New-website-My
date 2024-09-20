3374965acd9346d5f13de96b8b4cdef99b25a179
thecapturelife <albinjohn1996th@gmail.com>
2024-09-21 00:02:01 +0530

    first commit

---
+++ app/components/MainButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/MainButton.css'
+
+const MainButton: React.FC = () => {
+  return (
+    <button className="button">
+      Button
+    </button>
+  );
+};
+
+export default MainButton;
